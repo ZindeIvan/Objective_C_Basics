f0c717e6f9b50355dfde9723771175d1add6fc95
ZindeIvan <zinde.ivan@yandex.ru>
2020-12-27 20:50:08 +0600

    init lesson-7

---
+++ Lesson-7/Lesson-7/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Lesson-7
+//
+//  Created by Зинде Иван on 12/27/20.
+//  Copyright © 2020 Зинде Иван. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+
+@end
+
---
+++ Lesson-7/Lesson-7/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Lesson-7
+//
+//  Created by Зинде Иван on 12/27/20.
+//  Copyright © 2020 Зинде Иван. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
